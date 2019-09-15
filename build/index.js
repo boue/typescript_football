0cf9ad379cc9c25733fe07a154a4d9826506e53c
boue <boue.germain@gmail.com>
2019-09-15 16:04:32 -0700

    Add enum type MatchResult and print out the number of chelsea wins

--- build/index.js
+++ build/index.js
@@ -1,2 +1,32 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = __importDefault(require("fs"));
+// 2 dimensional array of strings
+var matches = fs_1.default
+    .readFileSync("football.csv", {
+    encoding: "utf-8"
+})
+    .split("\n")
+    .map(function (row) {
+    return row.split(",");
+});
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+var chelseaWins = 0;
+for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
+    var match = matches_1[_i];
+    if (match[1] === "Chelsea" && match[5] === MatchResult.HomeWin) {
+        chelseaWins++;
+    }
+    else if (match[2] === "Chelsea" && match[5] === MatchResult.AwayWin) {
+        chelseaWins++;
+    }
+}
+console.log(chelseaWins);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
 import fs from "fs";
 
+// 2 dimensional array of strings
+const matches = fs
+  .readFileSync("football.csv", {
+    encoding: "utf-8"
+  })
+  .split("\n")
+  .map((row: string): string[] => {
+    return row.split(",");
+  });
+
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D"
+}
+
+let chelseaWins = 0;
+
+for (let match of matches) {
+  if (match[1] === "Chelsea" && match[5] === MatchResult.HomeWin) {
+    chelseaWins++;
+  } else if (match[2] === "Chelsea" && match[5] === MatchResult.AwayWin) {
+    chelseaWins++;
+  }
+}
+
+console.log(chelseaWins);
