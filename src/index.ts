a9f99c6637b63c7219aa6c8b3d51a17e5716fcba
boue <boue.germain@gmail.com>
2019-09-19 16:54:40 -0700

    Create new CsvFileReader and pass in the football.csv file, create a new MatchReader instance and load up the data

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,15 @@ import { CsvFileReader } from "./CsvFileReader";
 import { MatchResult } from "./MatchResult";
 
 // create obj that satisfies 'DataReader' interface
-
+const csvFileReader = new CsvFileReader("football.csv");
 // create an instance of MatchReader and pass in something satisfying
 // the 'DataReader' Interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let chelseaWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === "Chelsea" && match[5] === MatchResult.HomeWin) {
     chelseaWins++;
   } else if (match[2] === "Chelsea" && match[5] === MatchResult.AwayWin) {
