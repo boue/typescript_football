4818b9e9bf2d5c9a0bb20dd6fd212e80aa9c3e0f
boue <boue.germain@gmail.com>
2019-09-19 16:47:14 -0700

    Refactor MatchReader to have a load function to it satisfies the interface

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -1,8 +1,33 @@
+import { dateStringToDate } from "./utils";
+import { MatchResult } from "./MatchResult";
+
+// defined a tuple MatchData
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
 interface DataReader {
   read(): void;
   data: string[][];
 }
 
 export class MatchReader {
+  matches: MatchData[] = [];
   constructor(public reader: DataReader) {}
+
+  load(): void {
+    this.reader.read();
+    // does not mutate, gives you a new array back
+    this.matches = this.reader.data.map(
+      (row: string[]): MatchData => {
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+          row[5] as MatchResult, //type assertion, override ts default
+          row[6]
+        ];
+      }
+    );
+  }
 }
